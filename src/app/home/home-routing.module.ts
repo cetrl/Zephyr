e9e5d471282c45b4c7331f33eb6170179b43640f
cetrl <celiatruel@gmail.com>
2024-08-29 10:26:12 +0200

    routes home/feeds

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { FeedListPage } from '../pages/feed-list/feed-list.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'feed',
+    component: FeedListPage,
   }
 ];
 
--- src/app/pages/feed-list/feed-list-routing.module.ts
+++ src/app/pages/feed-list/feed-list-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FeedListPage } from './feed-list.page';
+import { HomePage } from 'src/app/home/home.page';
 
 const routes: Routes = [
   {
     path: '',
     component: FeedListPage
+  },
+  {
+    path: '../home',
+    component: HomePage
   }
 ];
 
