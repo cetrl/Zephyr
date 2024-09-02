df56c02fed7694ff659047e9c5589fc5f8f9139b
cetrl <celiatruel@gmail.com>
2024-09-02 15:37:35 +0200

    routes home-feeds fixed + article list deleted

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,13 +5,14 @@ import { FeedService } from '../../services/feed.service';
 import { IonicModule } from '@ionic/angular';
 import { ArticlePreviewComponent } from '../../components/article-preview/article-preview.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, ArticlePreviewComponent]
+  imports: [IonicModule, CommonModule, ArticlePreviewComponent, RouterModule]
 })
 export class HomePage implements OnInit {
   recentArticles$!: Observable<Article[]>;
