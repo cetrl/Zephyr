45778872bcd4df5cee0b09464c7da14f31133204
cetrl <celiatruel@gmail.com>
2024-09-04 16:08:48 +0200

    c rangé

--- node_modules/@ionic/core/dist/esm/app-globals-5dbb61a5.js
+++
@@ -1,8 +0,0 @@
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-import { i as initialize } from './ionic-global-c81d82ab.js';
-
-const globalScripts = initialize;
-
-export { globalScripts as g };
